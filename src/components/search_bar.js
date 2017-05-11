fe57c86bd466df3d18b9799ae00127ad6d0a3f23
Aki Gao <xag9bb@virginia.edu>
2017-05-11 16:55:47 -0400

    complete more on state, connect input to searchbar state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,9 +5,13 @@ class SearchBar extends Component{
       super(props);
       this.state = {term: ''};
     }
-    
+
     render(){
-      return <input onChange={event => console.log(event.target.value)} />;
+      return(
+        <div>
+          <input onChange={event => this.setState({term: event.target.value})} />
+        </div>
+      );
     }
 
     onInputChange(event) { // or handleInputChange
