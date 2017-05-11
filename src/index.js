44eb20cd1635e89fc2d605278c5c523b29d5afa2
Aki Gao <xag9bb@virginia.edu>
2017-05-11 15:19:25 -0400

    complete class-based components, add search bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDOZVmlJpsTZy9ioUo3yXILgvbPH7cO3d4';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+  </div>
+  );
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
