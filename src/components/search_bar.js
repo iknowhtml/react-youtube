2ca24b86b3dbf5ff1d388275da5290bf32e65348
Aki Gao <xag9bb@virginia.edu>
2017-05-11 17:05:44 -0400

    set input value to state value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,14 +3,14 @@ import React, {Component} from 'react';
 class SearchBar extends Component{
     constructor(props){
       super(props);
-      this.state = {term: ''};
+      this.state = { term: '' };
     }
 
     render(){
       return(
         <div>
           <input
-            value={this.state.term} 
+            value={this.state.term}
             onChange={event => this.setState({term: event.target.value})} />
         </div>
       );
