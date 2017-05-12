bce6217cbf184409b128d18058badfab904798c3
Aki Gao <xag9bb@virginia.edu>
2017-05-12 19:49:12 -0400

    refactor index.js to index.jsx for reach auto completion purposes, convert app to class component, add video state

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDOZVmlJpsTZy9ioUo3yXILgvbPH7cO3d4';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+        this.setState({ videos }); //resolves to {'videos' : videos}, works when key and value are equal
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
