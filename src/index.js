693d53a9518f4fd2bcb09be750f8a80bf781730c
Aki Gao <xag9bb@virginia.edu>
2017-05-11 22:51:18 -0400

    complete youtube search response, add code to fetch results from youtube api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDOZVmlJpsTZy9ioUo3yXILgvbPH7cO3d4';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, data => console.log(data));
 const App = () => {
   return (
     <div>
