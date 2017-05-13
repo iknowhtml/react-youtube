224e7f504a9cda37f22187c9cb5b1766f5c0b895
Aki Gao <xag9bb@virginia.edu>
2017-05-12 22:57:34 -0400

    complete list item keys, add key to video list item in video list component

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import VideoList from './components/video_details';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
 
 const API_KEY = 'AIzaSyDOZVmlJpsTZy9ioUo3yXILgvbPH7cO3d4';
 
