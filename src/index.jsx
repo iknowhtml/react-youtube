d99bfc4be81d239a3cfead334d98f56bb5066de6
Aki Gao <xag9bb@virginia.edu>
2017-05-14 14:30:26 -0400

    change default search term, add default value to search bar

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import VideoList from './components/video_list';
 import VideoDetail from './components/video_details'
 
 const API_KEY = 'AIzaSyDOZVmlJpsTZy9ioUo3yXILgvbPH7cO3d4';
-
+const DEFAULT_TERM = 'Japan'
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,7 +17,7 @@ class App extends Component {
       selectedVideo: null,
      };
 
-     this.videoSearch('surfboards');
+     this.videoSearch(DEFAULT_TERM);
 
   }
 
@@ -38,7 +38,9 @@ class App extends Component {
     const videoSearch = _.debounce((term) => this.videoSearch(term), 300);
     return (
       <div>
-        <SearchBar onSearchTermChange={term => videoSearch(term)}/>
+        <SearchBar
+          defaultValue={DEFAULT_TERM}
+          onSearchTermChange={term => videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
